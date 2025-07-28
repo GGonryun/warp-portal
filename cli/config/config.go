f7008504064bd2d78733a018832961c443280d01
Miguel Campos <miguel.campos@p0.dev>
2025-07-28 11:50:16 -0700

    clean up urls

--- cli/config/config.go
+++ cli/config/config.go
@@ -6,7 +6,7 @@ const (
 	DefaultBranch     = "main"
 
 	// Registration configuration
-	RegistrationWebsite = "https://warp.portal.io/register"
+	RegistrationWebsite = "https://p0.app/register"
 
 	// CLI configuration
 	CLIName    = "warpportal"
